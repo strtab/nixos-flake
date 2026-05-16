887926763a56f2eeb67076c1d45f41c908ee6932
strtab <maleshkodaniil@gmail.com>
2026-05-16 16:15:27 +0300

    add appimage wrapper

--- mods/system/utils.nix
+++ mods/system/utils.nix
@@ -6,6 +6,11 @@
       enableSSHSupport = true;
     };
 
+    appimage = {
+      enable = true;
+      binfmt = true;
+    };
+
     mtr.enable = true;
   };
 
@@ -82,10 +87,24 @@
         xz
         systemd
 
+        pango
+        cairo
+        atk
+        gdk-pixbuf
+        fontconfig
+        freetype
+        dbus
+        alsa-lib
+        expat
+        libxkbcommon
+        fribidi
+        harfbuzz
+        libgpg-error
+        gmp
+
         brotli
         unixODBC
 
-        # My own additions
         xorg.libXcomposite
         xorg.libXtst
         xorg.libXrandr
@@ -136,18 +155,6 @@
         ffmpeg
         libudev0-shim
 
-        # needed to run unity
-        gtk3
-        icu
-        libnotify
-        gsettings-desktop-schemas
-        # https://github.com/NixOS/nixpkgs/issues/72282
-        # https://github.com/NixOS/nixpkgs/blob/2e87260fafdd3d18aa1719246fd704b35e55b0f2/pkgs/applications/misc/joplin-desktop/default.nix#L16
-        # log in /home/leo/.config/unity3d/Editor.log
-        # it will segfault when opening files if you don’t do:
-        # export XDG_DATA_DIRS=/nix/store/0nfsywbk0qml4faa7sk3sdfmbd85b7ra-gsettings-desktop-schemas-43.0/share/gsettings-schemas/gsettings-desktop-schemas-43.0:/nix/store/rkscn1raa3x850zq7jp9q3j5ghcf6zi2-gtk+3-3.24.35/share/gsettings-schemas/gtk+3-3.24.35/:$XDG_DATA_DIRS
-        # other issue: (Unity:377230): GLib-GIO-CRITICAL **: 21:09:04.706: g_dbus_proxy_call_sync_internal: assertion 'G_IS_DBUS_PROXY (proxy)' failed
-
         # Verified games requirements
         xorg.libXt
         xorg.libXmu
@@ -186,19 +193,6 @@
         librsvg
         xorg.libXft
         libvdpau
-        # ...
-        # Some more libraries that I needed to run programs
-        pango
-        cairo
-        atk
-        gdk-pixbuf
-        fontconfig
-        freetype
-        dbus
-        alsa-lib
-        expat
-        # for blender
-        libxkbcommon
 
         libxcrypt-legacy # For natron
         libGLU # For natron
