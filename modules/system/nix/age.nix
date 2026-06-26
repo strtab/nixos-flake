8aca0b5dbb7e315fd83cfa5e0deb4529a2f9c548
Maleshko Daniil Dmitrievich <maleshkodaniil@gmail.com>
2026-06-26 15:54:24 +0300

    change file permissions

--- modules/system/nix/age.nix
+++ modules/system/nix/age.nix
@@ -6,11 +6,16 @@
     #
     # To provision this key once:
     #   ssh-keygen -t ed25519 -f ~/.ssh/agenix
-    identityPaths = [ "${config.home-manager.users.${config.var.username}.home.homeDirectory}/.ssh/agenix" ];
+    identityPaths = [
+      "${config.home-manager.users.${config.var.username}.home.homeDirectory}/.ssh/agenix"
+    ];
     secrets = {
       personal-password.file = ../../../secrets/mail/personal-password.age;
       work-password.file = ../../../secrets/mail/work-password.age;
-      git-credentials.file = ../../../secrets/git-credentials.age;
+      git-credentials = {
+        file = ../../../secrets/git-credentials.age;
+        mode = "400";
+      };
     };
   };
 }
