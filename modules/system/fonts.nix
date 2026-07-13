4e1a5e41ceedd73509f3eb3c87f827a40f492913
Maleshko Daniil <maleshkodaniil@gmail.com>
2026-07-13 17:29:58 +0300

    update

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -4,13 +4,11 @@
     enableDefaultPackages = true;
     packages = with pkgs; [
       googlesans-code
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
       noto-fonts
 
-      roboto-serif
-      roboto-mono
-      roboto-slab
-      roboto-flex
-      roboto
+      geist-font
     ];
 
     fontDir.enable = true;
@@ -21,7 +19,7 @@
       defaultFonts = {
         monospace = ["JetBrainsMono Nerd Font" "Roboto Mono"];
         sansSerif = ["Google Sans Flex" "Noto Sans"];
-        serif = ["Tex Gyre Schola" "Roboto Serif" "Noto Serif"];
+        serif = ["Tex Gyre Schola" "Noto Serif"];
       };
     };
   };
