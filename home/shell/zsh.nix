757dca4ecfd0b71a5af36bcdc0b8e87fbfd2116b
Maleshko Daniil <maleshkodaniil@gmail.com>
2026-07-14 00:07:52 +0300

    feat: update highliting & add interactive comments

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -4,7 +4,21 @@
     enable = true;
     autocd = true;
     enableCompletion = true;
-    syntaxHighlighting.enable = true;
+    syntaxHighlighting = {
+      enable = true;
+      styles = {
+        command = "fg=white";
+        builtin = "fg=white";
+        alias = "fg=white";
+        global-alias = "fg=white";
+        unknown-token = "fg=white";
+        comment = "fg=8";
+        double-hyphen-option = "fg=2";
+        redirection = "fg=6";
+        function = "fg=6";
+        reserved-word = "fg=6";
+      };
+    };
     dotDir = "${config.xdg.configHome}/zsh";
     history = {
       path = "${config.xdg.configHome}/zsh/histfile";
@@ -13,6 +27,7 @@
     };
     initContent = ''
       PROMPT='%n@%m %1~ %# '
+      TERM=xterm-256color
 
       # Emacs keybinds
       bindkey -e
@@ -22,17 +37,19 @@
       zle -N edit-command-line
       bindkey "^E" edit-command-line
 
-      # Use cache
+      mkdir -p $XDG_CACHE_HOME/zsh &>/dev/null
+
       zstyle ':completion:*' use-cache on
       zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
-
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z} m:{A-Z}={a-z}'
+
+      setopt interactivecomments
 
       ${lib.optionalString config.programs.bat.enable ''
         alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
       ''};
 
-      if (( COLUMNS > 100 )); then 
+      if (( COLUMNS > 200 && LINES > 30 )); then 
         cat << 'EOF' > /dev/stdout
       ▲
       __  __                                     _            __
