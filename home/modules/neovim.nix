3753aff30ac06a76ca3be0dc449bfd9f92dc54cc
Maleshko Daniil <maleshkodaniil@gmail.com>
2026-07-14 23:34:40 +0300

    feat: setup lsp's & add git repo clonning script

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -1,11 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 {
   home.packages = with pkgs; [
-    roslyn-ls # csharp lsp
-    nil # nix lsp
     neovim
+
+    bash-language-server
+    yaml-language-server
+    lua-language-server
+    gopls # golang lsp
+    roslyn-ls # csharp lsp
+    nixfmt # nix formatter
+    nil # nix lsp
   ];
-  # programs.neovim = {
-  #   enable = true;
-  # };
+  home.activation.cloneNeovimDots = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  if [ ! -d "${config.home.homeDirectory}/.config/nvim" ]; then
+    $DRY_RUN_CMD ${pkgs.git}/bin/git clone --depth 1 --recursive $VERBOSE_ARG \
+      https://github.com/strtab/nvim-config.git "${config.home.homeDirectory}/.config/nvim"
+  fi
+'';
 }
