8a8968397f0b3d53beb46da77b2eed91b6ed3f8c
Maleshko Daniil <maleshkodaniil@gmail.com>
2026-07-22 00:43:08 +0300

    add activation script that copy repo if it doesn't exist

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   home.packages = with pkgs; [
     neovim
@@ -11,10 +16,10 @@
     nixfmt # nix formatter
     nil # nix lsp
   ];
-  home.activation.cloneNeovimDots = lib.hm.dag.entryAfter ["writeBoundary"] ''
-  if [ ! -d "${config.home.homeDirectory}/.config/nvim" ]; then
-    $DRY_RUN_CMD ${pkgs.git}/bin/git clone --depth 1 --recursive $VERBOSE_ARG \
-      https://github.com/strtab/nvim-config.git "${config.home.homeDirectory}/.config/nvim"
-  fi
-'';
+  home.activation.cloneNeovimDots = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ ! -d "${config.home.homeDirectory}/.config/nvim" ]; then
+      $DRY_RUN_CMD ${pkgs.git}/bin/git clone --depth 1 --recursive $VERBOSE_ARG \
+        https://github.com/strtab/nvim-config.git "${config.home.homeDirectory}/.config/nvim"
+    fi
+  '';
 }
