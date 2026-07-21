19b3496a594523f6ba732cf0256358adf9cb22a3
Maleshko Daniil <maleshkodaniil@gmail.com>
2026-07-22 00:39:14 +0300

    update

--- flake.nix
+++ flake.nix
@@ -20,10 +20,14 @@
     hypr-dynamic-cursors.url = "github:VirtCode/hypr-dynamic-cursors/da447486c84e0be81f2cdd208af1ef92469f0a88";
     hypr-dynamic-cursors.inputs.hyprland.follows = "hyprland";
 
-    dotfiles.url = "gitlab:strtab/illogical-impulce-dots/dbf7cb4ece8b4cd8a290fc5958095f2771254d7b";
+    # Kde Theme
+    geist.url = "github:strtab/Plasma-Geist-visual-style";
+    geist.flake = false;
+
+    dotfiles.url = "gitlab:strtab/illogical-impulce-dots/4f308b4366f3f8d665ba76037be095a58541be52";
     dotfiles.flake = false;
     illogical-flake = {
-      url = "gitlab:strtab/illogical-flake/c12a0cb061115572f875e6c6a1e01b5bb0c047a5";
+      url = "gitlab:strtab/illogical-flake/910eba218abc31adab2e685233cd80e22e61b823";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.dotfiles.follows = "dotfiles";
     };
