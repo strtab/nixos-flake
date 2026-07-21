6829b7bd741406923a179d5ee24559611b333772
Maleshko Daniil <maleshkodaniil@gmail.com>
2026-07-22 00:38:38 +0300

    update aliases

--- home/shell/aliases.nix
+++ home/shell/aliases.nix
@@ -1,19 +1,23 @@
 {
   home.shellAliases = {
     ":q" = "exit";
+    "v" = "nvim";
     "vi" = "nvim";
     "svi" = "sudo -e";
     "ll" = "ls --group-directories-first -lha";
     "l" = "ls --group-directories-first -lh";
     "la" = "ls --group-directories-first -ah";
+    "gl" = "git log --pretty=format:\"%h %s\" --graph";
+    "ga" = "git add";
+    "gs" = "git status";
+    "gc" = "git commit";
+    "gr" = "git restore --staged";
     "..." = "cd ../..";
-    "j" = "jobs";
     "ip" = "ip -c=auto";
-    "dua" = "du -s";
     "du" = "du -h";
     "ku" = "kubectl";
     "rr" = "echo \"$?\"";
     "sduo" = "sudo";
-    "clean" = "nh clean all --keep 3 --optimise; sync";
+    "clean" = "sync; nh clean all --keep 3 --optimise";
   };
 }
